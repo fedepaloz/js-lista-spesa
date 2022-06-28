fe7c26f02e33c4b350191ca4facd33dca87fb112
fedepaloz <palozzifederico@gmail.com>
2022-06-28 18:05:45 +0200

    ciclo for test

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("prova")
+//INIZIO CON IL CICLO FOR PER RIPASSARE COME SI FA
+//Uova,Pane,Latte,Biscotti,Pasta,Affettati,Formaggio,Verdura,Frutta
+
+//voglio metterli dentro la ul quindi la prendo dal dom
+const Spesa = document.getElementById("lista-spesa");
+
+//creo l'array con gli elementi della lista della spesa
+const listaProdotti = ['Uova','Pane','Latte','Biscotti','Pasta','Affettati','Formaggio','Verdura','Frutta'];
+
+//creo il ciclo for con gli elementi della lista della spesa
+
+let listaSpesa = '';
+
+for (let i = 0; i < listaProdotti.length; i++)
+{listaSpesa += <li> ${listaProdotti[i]} </li>}
+
+
+
+
